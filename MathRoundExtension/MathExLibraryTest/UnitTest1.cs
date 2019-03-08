310dbbbfe3222d0fb39cafdfbc962277c424ac09
Fragmented_nt <fragmentednt@zoho.com>
2019-03-08 14:58:28 +0900

    add: ソース・テストなどを追加

---
+++ MathRoundExtension/MathExLibraryTest/UnitTest1.cs
@@ -0,0 +1,117 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathRoundExtension;
+
+namespace MathExLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRound()
+        {
+            var vals = new[] { 0, 3.14159265, -3.14159265, 1000.01, -1000.01, 999.99999999, -999.99999999, 0.0010001, -0.0010001, 0.0000099999, -0.0000099999 };
+            var ans = new[] {  0, 3.142,      -3.142,      1000,    -1000,    1000,         -1000,         0.001000,  -0.001000,  0.0000100000, -0.0000100000 };
+            for (int i = 0; i < vals.Length; i++)
+            {
+                var result = MathEx.Round(vals[i], 4);
+                Assert.AreEqual(result, ans[i], $"Expected for '{ans[i]}': false, Actual:{result}");
+            }
+        }
+
+        [TestMethod]
+        public void TestRoundUp()
+        {
+            var vals = new[] { 0, 3.14159265, -3.14159265, 1000.01, -1000.01, 999.99999999, -999.99999999, 0.0010001, -0.0010001, 0.0000099999, -0.0000099999 };
+            var ans = new[] {  0, 3.142,      -3.141,      1001,    -1000,    1000,         -999.9,        0.0010010, -0.0010000, 0.0000100000, -0.0000099990 };
+            for (int i = 0; i < vals.Length; i++)
+            {
+                var result = MathEx.RoundUp(vals[i], 4);
+                Assert.AreEqual(result, ans[i], $"Expected for '{ans[i]}': false, Actual:{result}");
+            }
+        }
+        
+        [TestMethod]
+        public void TestRoundDown()
+        {
+            var vals = new[] { 0, 3.14159265, -3.14159265, 1000.01, -1000.01, 999.99999999, -999.99999999, 0.0010001, -0.0010001, 0.0000099999, -0.0000099999 };
+            var ans = new[] {  0, 3.141,      -3.142,      1000,    -1001,    999.9,        -1000,         0.0010000, -0.0010010, 0.0000099990, -0.0000100000 };
+            for (int i = 0; i < vals.Length; i++)
+            {
+                var result = MathEx.RoundDown(vals[i], 4);
+                Assert.AreEqual(result, ans[i], $"Expected for '{ans[i]}': false, Actual:{result}");
+            }
+        }
+
+        [TestMethod]
+        public void TestTrimUp()
+        {
+            var vals = new[] { 0, 3.14159265, -3.14159265, 1000.01, -1000.01, 999.99999999, -999.99999999, 0.0010001, -0.0010001, 0.0000099999, -0.0000099999 };
+            var ans = new[] {  0, 3.142,      -3.142,      1001,    -1001,    1000,         -1000,         0.0010010, -0.0010010, 0.0000100000, -0.0000100000 };
+            for (int i = 0; i < vals.Length; i++)
+            {
+                var result = MathEx.TrimUp(vals[i], 4);
+                Assert.AreEqual(result, ans[i], $"Expected for '{ans[i]}': false, Actual:{result}");
+            }
+        }
+
+        [TestMethod]
+        public void TestTrimDown()
+        {
+            var vals = new[] { 0, 3.14159265, -3.14159265, 1000.01, -1000.01, 999.99999999, -999.99999999, 0.0010001, -0.0010001, 0.0000099999, -0.0000099999 };
+            var ans = new[] {  0, 3.141,      -3.141,      1000.00, -1000.00, 999.9,        -999.9,        0.0010000, -0.0010000, 0.0000099990, -0.0000099990 };
+            for (int i = 0; i < vals.Length; i++)
+            {
+                var result = MathEx.TrimDown(vals[i], 4);
+                Assert.AreEqual(result, ans[i], $"Expected for '{ans[i]}': false, Actual:{result}");
+            }
+        }
+
+        [TestMethod]
+        public void TestNormalize()
+        {
+            var vals = new[] { 0, 3.14159265, -3.14159265, 1000.01, -1000.01, 999.99999999, -999.99999999, 0.0010001, -0.0010001, 0.0000099999, -0.0000099999 };
+            var ans = new[] {  0, 3.14159265,  3.14159265, 1.00001,  1.00001, 9.9999999999,  9.9999999999, 1.0001000,  1.0001000, 9.9999000000,  9.9999000000 };
+            for (int i = 0; i < vals.Length; i++)
+            {
+                var result = MathEx.Normalize(vals[i]);
+                Assert.AreEqual(result, ans[i], $"Expected for '{ans[i]}': false, Actual:{result}");
+            }
+        }
+
+        [TestMethod]
+        public void TestHeadValue()
+        {
+            var vals = new[] { 0, 3.14159265, -3.14159265, 1000.01, -1000.01, 999.99999999, -999.99999999, 0.0010001, -0.0010001, 0.0000099999, -0.0000099999 };
+            var ans = new[] {  0, 31,          31,         10,       10,       99,            99,          10,         10,        99,            99           };
+            for (int i = 0; i < vals.Length; i++)
+            {
+                var result = MathEx.HeadValue(vals[i], 2);
+                Assert.AreEqual(result, ans[i], $"Expected for '{ans[i]}': false, Actual:{result}");
+            }
+        }
+
+        [TestMethod]
+        public void TestSignificantDigits()
+        {
+            var vals = new[] { 0, 3.14159265, -3.14159265, 1000.01, -1000.01, 999.99999999, -999.99999999, 0.0010001, -0.0010001, 0.0000099999, -0.0000099999 };
+            var ans = new[] {  0, 9,           9,          6,        6,       11,           11,            5,         5,          5,            5 };
+            for (int i = 0; i < vals.Length; i++)
+            {
+                var result = MathEx.SignificantDigits(vals[i]);
+                Assert.AreEqual(result, ans[i], $"Expected for '{ans[i]}': false, Actual:{result}");
+            }
+        }
+
+        [TestMethod]
+        public void TestDigit()
+        {
+            var vals = new[] { 0, 3.14159265, -3.14159265, 1000.01, -1000.01, 999.99999999, -999.99999999, 0.0010001, -0.0010001, 0.0000099999, -0.0000099999 };
+            var ans = new[] {  0, 0,          0,           3,       3,        2,            2,             -3,        -3,         -6,           -6 };
+            for (int i = 0; i < vals.Length; i++)
+            {
+                var result = MathEx.Digit(vals[i]);
+                Assert.AreEqual(result, ans[i]);
+            }
+        }
+    }
+}
---
+++ MathRoundExtension/MathRoundExtension/MathRoundExtension.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Linq;
+
+namespace MathRoundExtension
+{
+    public static class MathEx
+    {
+        /// <summary>
+        /// 有効桁数を指定しての丸め
+        /// </summary>
+        /// <param name="val">丸め対象の数値</param>
+        /// <param name="digit">有効桁数</param>
+        /// <param name="midpointRounding">丸め方向の指示</param>
+        /// <returns>丸め結果</returns>
+        public static double Round(double val,
+                                   int digit,
+                                   MidpointRounding midpointRounding = MidpointRounding.AwayFromZero)
+        {
+            int dig = -Digit(val) + (digit - 1);
+            if (0 <= dig && dig <= 15)
+                return Math.Round(val, dig, midpointRounding);
+            double coef = Math.Pow(10, dig);
+            return Math.Round(val * coef, midpointRounding) / coef;
+        }
+
+        /// <summary>
+        /// 有効桁数を指定しての切り上げ
+        /// </summary>
+        /// <param name="val">切り上げ対象の数値</param>
+        /// <param name="digit">有効桁数</param>
+        /// <returns>切り上げ結果</returns>
+        public static double RoundUp(double val, int digit)
+        {
+            double coef = Math.Pow(10, -Digit(val) + (digit - 1));
+            return Math.Ceiling(val * coef) / coef;
+        }
+
+        /// <summary>
+        /// 有効桁数を指定しての切り捨て
+        /// </summary>
+        /// <param name="val">切り捨て対象の数値</param>
+        /// <param name="digit">有効桁数</param>
+        /// <returns>切り捨て結果</returns>
+        public static double RoundDown(double val, int digit)
+        {
+            double coef = Math.Pow(10, -Digit(val) + (digit - 1));
+            return Math.Floor(val * coef) / coef;
+        }
+
+        /// <summary>
+        /// 有効桁数を指定しての切り上げ（絶対値が0から遠のく方向への切り上げ）
+        /// </summary>
+        /// <param name="val">切り上げ対象の数値</param>
+        /// <param name="digit">有効桁数</param>
+        /// <returns>切り上げ結果</returns>
+        public static double TrimUp(double val, int digit)
+        {
+            double coef = Math.Pow(10, -Digit(val) + (digit - 1));
+            return val < 0 ? Math.Floor(val * coef) / coef
+                           : Math.Ceiling(val * coef) / coef;
+        }
+
+        /// <summary>
+        /// 有効桁数を指定しての切り捨て（絶対値が0に近づく方向への切り捨て）
+        /// </summary>
+        /// <param name="val">切り捨て対象の数値</param>
+        /// <param name="digit">有効桁数</param>
+        /// <returns>切り捨て結果</returns>
+        public static double TrimDown(double val, int digit)
+        {
+            double coef = Math.Pow(10, -Digit(val) + (digit - 1));
+            return val < 0 ? Math.Ceiling(val * coef) / coef
+                           : Math.Floor(val * coef) / coef;
+        }
+
+        /// <summary>
+        /// 数値に対して先頭から指定した桁数のみ抜き出し
+        /// </summary>
+        /// <remarks>符号は失われる</remarks>
+        /// <param name="val">抜き出し対象の数値</param>
+        /// <param name="digit">桁数</param>
+        /// <returns>抜き出し結果</returns>
+        public static int HeadValue(double val, int digit = 1)
+        {
+            return ((int)RoundDown(Normalize(val) * Math.Pow(10, digit), digit)) / 10;
+        }
+
+        /// <summary>
+        /// 有効桁数の判定
+        /// </summary>
+        /// <remarks>テスト不十分</remarks>
+        /// <param name="val">有効桁数判定対象</param>
+        /// <returns>有効桁数</returns>
+        public static int SignificantDigits(double val)
+        {
+            return new String(Normalize(val).ToString("F16")
+                                            .Where((c, i) => (c != '0' || i != 0) && c != '.')
+                                            .ToArray())
+                                            .LastIndexOfAny("123456789".ToArray()) + 1;
+        }
+
+        /// <summary>
+        /// 数値をx.xxxxの形式に整える
+        /// </summary>
+        /// <remarks>符号は失われる</remarks>
+        /// <param name="val">整形対象</param>
+        /// <returns>整形後数値</returns>
+        public static double Normalize(double val)
+        {
+            return Math.Abs(val) / Math.Pow(10, Digit(val));
+        }
+
+        /// <summary>
+        /// 桁数を求める（Log10nを計算）
+        /// </summary>
+        /// <remarks>0を渡された場合は0を返す</remarks>
+        /// <param name="val"></param>
+        /// <returns>桁数</returns>
+        public static int Digit(double val)
+        {
+            double abs = Math.Abs(val);
+            if (abs == 0)
+                return 0;
+            else
+                return (int)Math.Floor(Math.Log10(abs));
+        }
+    }
+}
